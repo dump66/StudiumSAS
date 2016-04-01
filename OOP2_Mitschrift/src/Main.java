6098b2c022a89bedd2bfe37573edfb08bd4fb93e
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-04-01 11:20:47 +0200

    Stage open and close

--- OOP2_Mitschrift/src/Main.java
+++ OOP2_Mitschrift/src/Main.java
@@ -5,34 +5,57 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 // Vom 01.04.16
 public class Main extends Application{
 	
-	Button button;
+	Button button, button2;
+	Stage stage;
+	
 	
 	public Main(){
 		System.out.println("1. Code der beim Erstellen des Objekts ausgeführt wird");
 	}
 	
 	public void init(){
+		
+		
 		Platform.setImplicitExit(false);
+		
 		System.out.println("2. Do initialisation here.");
 		button = new Button("Close");
 		button.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
 			public void handle(ActionEvent event) {
-				Platform.exit();
+				stage.show();
+			}
+		});
+		button2 = new Button("Schliessen");
+		button2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				stage.close();
 			}
 		});
 	}
 	
 	public void start(Stage primaryStage){
 		try{
+			stage = new Stage(StageStyle.UNDECORATED);
+			StackPane stackPane = new StackPane();
+			Text text = new Text("Kindfenster");
+			stackPane.getChildren().add(text);
+			Scene scene2 = new Scene(stackPane);
+			stage.setScene(scene2);
+			
 			BorderPane root = new BorderPane();
 			root.setCenter(button);
+			root.setRight(button2);
 			Scene scene = new Scene(root, 400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
