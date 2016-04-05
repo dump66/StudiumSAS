d4e74667dea50c0aada8dc744015f5d3d4fa14e0
Andy <andyschultze@hotmail.com>
2016-04-05 10:57:20 +0200

    Update Aufgabe1.java

--- OOP2_U1/src/Aufgabe1.java
+++ OOP2_U1/src/Aufgabe1.java
@@ -8,7 +8,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-// Test1
+// Test
 public class Aufgabe1 extends Application {
 	private int counter = 0;
 
