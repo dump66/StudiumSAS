bd1d7c5b90988317b5f7d5ebc04e746d4b73f740
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-04-08 10:17:12 +0200

    Aufgeräumt

--- OOP2_Mitschrift/src/Vorlesung010416.java
+++ OOP2_Mitschrift/src/Vorlesung010416.java
@@ -11,25 +11,23 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 // Vom 01.04.16
-public class Vorlesung010416 extends Application{
-	
+public class Vorlesung010416 extends Application {
+
 	Button button, button2;
 	Stage stage;
-	
-	
-	public Vorlesung010416(){
+
+	public Vorlesung010416() {
 		System.out.println("1. Code der beim Erstellen des Objekts ausgeführt wird");
 	}
-	
-	public void init(){
-		
-		
+
+	public void init() {
+
 		Platform.setImplicitExit(false);
-		
+
 		System.out.println("2. Do initialisation here.");
 		button = new Button("open");
 		button.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			@Override
 			public void handle(ActionEvent event) {
 				stage.show();
@@ -43,36 +41,36 @@ public class Vorlesung010416 extends Application{
 			}
 		});
 	}
-	
-	public void start(Stage primaryStage){
-		try{
+
+	public void start(Stage primaryStage) {
+		try {
 			stage = new Stage(StageStyle.UNDECORATED);
 			StackPane stackPane = new StackPane();
 			Text text = new Text("Kindfenster");
 			stackPane.getChildren().add(text);
 			Scene scene2 = new Scene(stackPane);
 			stage.setScene(scene2);
-			
+
 			BorderPane root = new BorderPane();
 			root.setCenter(button);
 			root.setRight(button2);
-			Scene scene = new Scene(root, 400,400);
+			Scene scene = new Scene(root, 400, 400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-		} catch (Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("3. Starten der Anwendung und Öffnen des Fenster.");
 		System.out.println("4. Schließen der Anwendung durch Fenster " + Platform.isImplicitExit());
 	}
-	
-	public void stop(){
+
+	public void stop() {
 		System.out.println("5. Code der beim Beenden der Anwendung ausgeführt wird.");
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		Vorlesung010416.launch(args);
 	}
 }
