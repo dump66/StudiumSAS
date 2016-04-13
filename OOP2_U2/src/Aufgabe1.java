a06e820cd7343da28d1452234710d3745ca9ae35
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-04-13 09:09:31 +0200

    Aufgabe 1 fertig

--- OOP2_U2/src/Aufgabe1.java
+++ OOP2_U2/src/Aufgabe1.java
@@ -1,9 +1,39 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-public class Aufgabe1 {
+public class Aufgabe1 extends Application {
+
+	public void start(Stage primStage) {
+		final Pane root = new Pane();
+		Scene scene = new Scene(root);
+		scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+				makeDot(root, event);
+			}
+
+
+		});
+
+		primStage.setScene(scene);
+		primStage.show();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
+	}
+	
+	private void makeDot(final Pane root, MouseEvent event) {
+		Circle circle = new Circle(5);
+		circle.setFill(Color.RED);
+		root.getChildren().add(circle);
+		circle.setCenterX(event.getSceneX());
+		circle.setCenterY(event.getSceneY());
 	}
 
 }
