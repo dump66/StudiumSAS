f14c8bccd31e46a9e0fcbb429b68486b11437990
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-04-29 10:04:01 +0200

    Vorlesung: WindowEvent Aufgabe8: Erstes Konzept

---
+++ OOP2_Mitschrift/src/Vorlesung290416_1.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+//WindowEvents
+public class Vorlesung290416_1 extends Application {
+
+	@Override
+	public void start(Stage prim) throws Exception {
+		StackPane root = new StackPane();
+		Button btn = new Button("Close");
+		root.getChildren().add(btn);
+		Scene scene = new Scene(root,400,300);
+		
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent arg0) {
+				WindowEvent windowEvent = new WindowEvent(prim, WindowEvent.WINDOW_CLOSE_REQUEST);
+				windowEvent.fireEvent(prim, windowEvent);
+			}
+		});
+		
+		prim.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			public void handle(WindowEvent event){
+				System.out.println("Do you really want to close?");
+				event.consume();
+			}
+		});
+		
+		prim.setScene(scene);
+		prim.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
