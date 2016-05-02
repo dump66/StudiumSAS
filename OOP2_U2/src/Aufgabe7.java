85de26b15c7251748cd4ab1f506d45565cc2efb3
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-05-02 15:25:45 +0200

    Aufgabe 8 vorläufig fertig

--- OOP2_U2/src/Aufgabe7.java
+++ OOP2_U2/src/Aufgabe7.java
@@ -8,7 +8,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.FlowPane;
@@ -29,11 +28,11 @@ public class Aufgabe7 extends Application {
 	private FlowPane fPane;
 	private VBox vbox;
 	private Scene scene;
-	Image selectedIm;
-	double objX;
-	double objY;
-	double mouseX;
-	double mouseY;
+	private Image selectedIm;
+	private double mX;
+	private double mY;
+	private double oX;
+	private double oY;
 
 	public void init() {
 		vbox = new VBox();
@@ -52,14 +51,17 @@ public class Aufgabe7 extends Application {
 
 	public void start(Stage primStage) {
 		EventHandler<MouseEvent> dragDetectedHandler = new EventHandler<MouseEvent>() {
+
 			public void handle(MouseEvent event) {
-				if (event.getTarget() instanceof ImageView) {
-					ImageView cloth = (ImageView) event.getTarget();
-					selectedIm = cloth.getImage();
-					ClipboardContent content = new ClipboardContent();
-					Dragboard db = cloth.startDragAndDrop(TransferMode.COPY);
-					content.putImage(cloth.getImage());
-					db.setContent(content);
+				if (!event.isAltDown()) {
+					if (event.getTarget() instanceof ImageView) {
+						ImageView cloth = (ImageView) event.getTarget();
+						selectedIm = cloth.getImage();
+						ClipboardContent content = new ClipboardContent();
+						Dragboard db = cloth.startDragAndDrop(TransferMode.COPY);
+						content.putImage(cloth.getImage());
+						db.setContent(content);
+					}
 				}
 			}
 		};
@@ -72,65 +74,82 @@ public class Aufgabe7 extends Application {
 			}
 		};
 
+		EventHandler<DragEvent> newDragOverHandler = new EventHandler<DragEvent>() {
+			public void handle(DragEvent event) {
+				if (!event.getDragboard().hasImage()) {
+					event.acceptTransferModes(TransferMode.MOVE);
+				}
+			}
+		};
+
 		EventHandler<DragEvent> dragDroppedHandler = new EventHandler<DragEvent>() {
 			public void handle(DragEvent event) {
-				for (Node n : sPane.getChildren()) {
-					if (n != dollIv) {
-						sPane.getChildren().remove(n);
-						break;
-					}
-				}
-				Image im = event.getDragboard().getImage();
-				for (int i = 0; i < pDresses.length; i++) {
-					if (selectedIm == pDresses[i].getImage()) {
-						sPane.getChildren().add(eDresses[i]);
-						switch (i) {
-						case 0:
-							scene.setFill(Color.GRAY);
-							break;
-						case 1:
-							scene.setFill(Color.CHARTREUSE);
-							break;
-						case 2:
-							scene.setFill(Color.YELLOW);
-							break;
-						case 3:
-							scene.setFill(Color.AQUAMARINE);
+				if (event.getTarget() instanceof ImageView) {
+					for (Node n : sPane.getChildren()) {
+						if (n != dollIv) {
+							sPane.getChildren().remove(n);
 							break;
 						}
-						break;
 					}
+					Image im = event.getDragboard().getImage();
+					for (int i = 0; i < pDresses.length; i++) {
+						if (selectedIm == pDresses[i].getImage()) {
+							sPane.getChildren().add(eDresses[i]);
+							switch (i) {
+							case 0:
+								scene.setFill(Color.GRAY);
+								break;
+							case 1:
+								scene.setFill(Color.CHARTREUSE);
+								break;
+							case 2:
+								scene.setFill(Color.YELLOW);
+								break;
+							case 3:
+								scene.setFill(Color.AQUAMARINE);
+								break;
+							default:
+								scene.setFill(Color.WHITE);
+								break;
+							}
+							break;
+						} else {
+							scene.setFill(Color.WHITE);
+						}
+					}
+					event.setDropCompleted(true);
 				}
-				event.setDropCompleted(true);
+			}
+		};
+
+		EventHandler<MouseEvent> mouseDraggedHandler = new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+				if (event.isAltDown()) {
+					primStage.setX(event.getScreenX() - (mX - oX));
+					primStage.setY(event.getScreenY() - (mY - oY));
+				}
 			}
 		};
 
 		EventHandler<MouseEvent> mousePressedHandler = new EventHandler<MouseEvent>() {
 			public void handle(MouseEvent event) {
-				objX = primStage.getX();
-				objY = primStage.getY();
-				mouseX = event.getScreenX();
-				mouseY = event.getScreenY();
+				mX = event.getScreenX();
+				mY = event.getScreenY();
+				oX = primStage.getX();
+				oY = primStage.getY();
 			}
 		};
 
-		EventHandler<MouseEvent> mouseDragHandler = new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent event) {
-				if (event.isAltDown()) {
-					primStage.setX(event.getScreenX() - (mouseX - objX));
-					primStage.setY(event.getScreenY() - (mouseY - objY));
-				}
-
-			}
-		};
-		 for (ImageView iv : pDresses) {
-		 iv.setOnDragDetected(dragDetectedHandler);
-		 }
+		// for (ImageView iv : pDresses) {
+		// iv.setOnDragDetected(dragDetectedHandler);
+		// }
 		scene = new Scene(vbox);
-		scene.setOnMouseDragged(mouseDragHandler);
-		scene.setOnMousePressed(mousePressedHandler);
 		sPane.setOnDragOver(dragOverHandler);
 		sPane.setOnDragDropped(dragDroppedHandler);
+		scene.setOnDragDetected(dragDetectedHandler);
+		// scene.setOnDragOver(newDragOverHandler);
+		scene.setOnMouseDragged(mouseDraggedHandler);
+		scene.setOnMousePressed(mousePressedHandler);
 
 		sPane.getChildren().add(dollIv);
 		fPane.getChildren().addAll(pDresses);
