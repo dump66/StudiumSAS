aa465c44b3a9293d47769507600357f5c0ed24e5
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-05-31 10:24:23 +0200

    Aufgabe1 fertig

--- OOP2_U4/src/Aufgabe1.java
+++ OOP2_U4/src/Aufgabe1.java
@@ -1,11 +1,19 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Aufgabe1 extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		WebView wv = new WebView();
+		WebEngine we = wv.getEngine();
+		we.load("http://www.google.de");
+		Scene scene = new Scene(wv);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
