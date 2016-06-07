7dfc9e968a2f8e28286e9b78a346a07292316a98
Andreas.Schultze <Andreas.Schultze@VI201504>
2016-06-07 14:33:36 +0200

    Bis Aufgabe 6 inkl. komplett

--- OOP2_U4/src/Aufgabe1.java
+++ OOP2_U4/src/Aufgabe1.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -11,38 +12,36 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebErrorEvent;
+import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Aufgabe1 extends Application {
 
 	WebView wv = new WebView();
+	boolean isTFFocused = false;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane root = new BorderPane();
 
 		ToolBar workBar = new ToolBar();
+		Button home = new Button("Home");
 		TextField tf = new TextField();
 		Button go = new Button("Go");
 		Button newTab = new Button("Tab");
-		workBar.getItems().addAll(tf, new Separator(), go, new Separator(), newTab);
-		
+		workBar.getItems().addAll(home, new Separator(), tf, new Separator(), go, new Separator(), newTab);
+
 		ToolBar statusBar = new ToolBar();
 		ProgressBar pBar = new ProgressBar();
-		pBar.progressProperty().addListener(new ChangeListener<Number>() {
 
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if (newValue.doubleValue() == 1){
-					pBar.setVisible(false);
-				} else {
-					pBar.setVisible(true);
-				}
-			}
-		});
-		statusBar.getItems().addAll(pBar);
+		Text alertText = new Text();
+		Text errorText = new Text();
+		statusBar.getItems().addAll(pBar, new Separator(), alertText, new Separator(), errorText);
 
 		TabPane tabBar = new TabPane();
 
@@ -52,6 +51,8 @@ public class Aufgabe1 extends Application {
 		Scene scene = new Scene(root, 1000, 700);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		
 
 		newTab.setOnAction(new EventHandler<ActionEvent>() {
 
@@ -78,10 +79,35 @@ public class Aufgabe1 extends Application {
 			@Override
 			public void changed(ObservableValue<? extends Tab> observable, Tab oldValue, Tab newValue) {
 				pBar.progressProperty().unbind();
+				wv.getEngine().setOnAlert(null);
+				wv.getEngine().setOnError(null);
 				if (newValue != null) {
 					wv = (WebView) newValue.getContent();
 					tf.setText(wv.getEngine().getLocation());
 					pBar.progressProperty().bind(wv.getEngine().getLoadWorker().progressProperty());
+					wv.getEngine().setOnAlert(new EventHandler<WebEvent<String>>() {
+
+						@Override
+						public void handle(WebEvent<String> event) {
+							alertText.setText(event.getData());
+						}
+					});
+					wv.getEngine().setOnError(new EventHandler<WebErrorEvent>() {
+
+						@Override
+						public void handle(WebErrorEvent event) {
+							errorText.setText(event.getMessage());
+						}
+					});
+					wv.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+
+						@Override
+						public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
+							if (newValue == State.FAILED) {
+								wv.getEngine().loadContent("<body>Fehler: Diese Seite ist nicht bekannt!</body>");
+							}
+						}
+					});
 				} else {
 					wv = null;
 				}
@@ -93,15 +119,59 @@ public class Aufgabe1 extends Application {
 			@Override
 			public void handle(ActionEvent event) {
 				if (wv != null) {
-					wv.getEngine().load(tf.getText());
+					String url = tf.getText();
+					if (!url.startsWith("http://www.")){
+						url = "http://www.".concat(url);
+					}
+					wv.getEngine().load(url);
+					tf.setText(url);
 				}
 			}
 		};
 
+		home.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				wv.getEngine().load("http://www.google.de");
+			}
+		});
+
+		pBar.progressProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				if (newValue.doubleValue() == 1) {
+					pBar.setVisible(false);
+				} else {
+					pBar.setVisible(true);
+				}
+			}
+		});
+		
+		tf.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				if (isTFFocused){
+					tf.selectAll();
+				}
+				isTFFocused = false;
+			}
+		});
+		
+		tf.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if (!oldValue && newValue){
+					isTFFocused = true;
+				}
+			}
+		});
+
 		tf.setOnAction(action);
-
 		go.setOnAction(action);
-
 		newTab.fire();
 	}
 
